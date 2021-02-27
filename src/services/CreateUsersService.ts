ce360ab2de53b7bb7c3181a1df78cc50fec10680
Trouttt <44469295+Trouttt@users.noreply.github.com>
2021-02-27 18:31:52 -0300

    Update: service CreateUsersService.ts. this service will create a new user.

--- src/services/CreateUsersService.ts
+++ src/services/CreateUsersService.ts
@@ -1,27 +1,28 @@
 import { getRepository } from 'typeorm';
 
 import User from '../models/User';
-import usersRouter from '../routes/users.routes';
 interface Request{
     name:string;
     email:string;
 }
 
 class CreateUserService {
+
     public async execute({name, email}:Request):Promise<User>{
         const usersRepository = getRepository(User);
         
-        const checkEmailExists = usersRepository.findOne({
-            where: email
+        const checkEmailExists = await usersRepository.findOne({
+            where: { email }
         });
 
         if(checkEmailExists){
+            console.log(checkEmailExists);
             throw new Error ('Email address already exists');
         }
 
         const user = usersRepository.create({
-            name:name,
-            email:email
+            name,
+            email
         });
 
         await usersRepository.save(user);
